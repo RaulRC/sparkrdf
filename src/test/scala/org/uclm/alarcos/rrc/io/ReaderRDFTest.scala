aeb5f7416ceb88103c2b63720393f087e2758e31
Raúl Reguillo Carmona <raul.reguillo@bbva.com>
2017-09-14 15:21:59 +0200

    [IMP] update testing for expandNnodes

--- src/test/scala/org/uclm/alarcos/rrc/io/ReaderRDFTest.scala
+++ src/test/scala/org/uclm/alarcos/rrc/io/ReaderRDFTest.scala
@@ -30,14 +30,50 @@ class ReaderRDFTest extends CommonTest with SparkSpec with MockFactory {
 
     val step = MockedTripleReader
     val graph = step.loadGraph(spark, testPath)
-    val depth = 3
+    val depth = 4
 
     val result = step.expandNodesNLevel(graph.vertices, graph, depth)
 
     graph.vertices.collect().foreach(println(_))
+    graph.edges.collect().foreach(println(_))
+    println("--")
     result.collect().foreach(println(_))
 
-    assert(true)
+    val results = result.collect()
+//    A -> B -> D -> F -> G
+//         | \  |
+//         v  \,v
+//         C -> E
+
+    //Nodes IDS
+    val A = 293150257L
+    val B = 293150288L
+    val C = 293150319L
+    val D = 293150350L
+    val E = 293150381L
+    val F = 293150412L
+    val G = 293150443L
+
+
+    //LVL 0
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === A & l.get(1).asInstanceOf[Long] === B & l.get(2) === 0).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === B & l.get(1).asInstanceOf[Long] === C & l.get(2) === 0).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === B & l.get(1).asInstanceOf[Long] === D & l.get(2) === 0).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === B & l.get(1).asInstanceOf[Long] === E & l.get(2) === 0).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === C & l.get(1).asInstanceOf[Long] === E & l.get(2) === 0).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === D & l.get(1).asInstanceOf[Long] === F & l.get(2) === 0).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === F & l.get(1).asInstanceOf[Long] === G & l.get(2) === 0).length === 1)
+    //LVL 1
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === A & l.get(1).asInstanceOf[Long] === C & l.get(2) === 1).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === A & l.get(1).asInstanceOf[Long] === D & l.get(2) === 1).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === A & l.get(1).asInstanceOf[Long] === E & l.get(2) === 1).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === B & l.get(1).asInstanceOf[Long] === F & l.get(2) === 1).length === 1)
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === D & l.get(1).asInstanceOf[Long] === G & l.get(2) === 1).length === 1)
+    //LVL 2
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === A & l.get(1).asInstanceOf[Long] === F & l.get(2) === 2).length === 1)
+    //LVL 3
+    assert(results.filter(l => l.get(0).asInstanceOf[Long] === A & l.get(1).asInstanceOf[Long] === G & l.get(2) === 3).length === 1)
+
 
   }
 }
